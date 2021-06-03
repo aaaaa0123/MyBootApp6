f146d87fdc4007ce7a06fce99c3e88fe853958d5
180510 <180510@e0420-05.JC-21.local>
2021-06-03 13:33:49 +0900

    commit

--- src/test/java/jp/te4a/spring/boot/myapp08/MyBootApp08ApplicationTests.java
+++ src/test/java/jp/te4a/spring/boot/myapp08/MyBootApp08ApplicationTests.java
@@ -1,10 +1,10 @@
-package jp.te4a.spring.boot.myapp07;
+package jp.te4a.spring.boot.myapp08;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MyBootApp07ApplicationTests {
+class MyBootApp08ApplicationTests {
 
 	@Test
 	void contextLoads() {
