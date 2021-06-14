e888523bd4cb2048582d29284b037cd016f34d48
180510 <180510@e0420-05.JC-21.local>
2021-06-14 12:57:29 +0900

    commit

---
+++ src/main/java/jp/te4a/spring/boot/myapp9/BookService.java
@@ -0,0 +1,68 @@
+package jp.te4a.spring.boot.myapp9;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
+public class BookService {
+	@Autowired
+	BookRepository bookRepository;
+	public BookForm create(BookForm bookForm) {
+		BookBean bookBean = new BookBean();
+		BeanUtils.copyProperties(bookForm, bookBean);
+		bookRepository.save(bookBean);
+		return bookForm;
+	}
+
+	public BookForm update(BookForm bookForm) {
+		BookBean bookBean = new BookBean();
+		BeanUtils.copyProperties(bookForm, bookBean);
+		bookRepository.save(bookBean);
+		return bookForm;
+	}
+	
+	
+
+
+	public void delete(Integer id) {
+
+		BookBean bookb = new BookBean();
+		bookb.setId(id); 
+		bookRepository.delete(bookb); 
+
+	}
+	public List<BookForm> findAll() {
+		List<BookBean> beanList = bookRepository.findAll();
+		List<BookForm> formList = new ArrayList<BookForm>();
+		for(BookBean bookBean: beanList) {
+			BookForm bookForm = new BookForm();
+			BeanUtils.copyProperties(bookBean, bookForm);
+			formList.add(bookForm);
+		}
+		return formList;
+	}
+	public BookForm findOne(Integer id) {  
+		//BookBean booka = new BookBean();
+		//booka.setId(id);
+		//String str = String.valueOf(booka);
+		//int num = Integer.parseInt(str);
+		Optional<BookBean> bookBean = bookRepository.findById(id);
+	    BookForm bookForm = new BookForm();
+	    BeanUtils.copyProperties(bookBean, bookForm);
+	    return bookForm;
+        /*
+		BookBean booka = new BookBean();
+		booka.setId(id);
+		BookBean bookBean = findById(booka);
+		BookForm bookForm = new BookForm();
+		BeanUtils.copyProperties(bookBean, bookForm);
+		return bookForm;
+		*/
+	}
+}
+
+
